69b638e18892464c8acfaf243bc3891b4721bc5d
Robert Lönnqvist <robert.lonnqvist@gmail.com>
2021-02-28 19:02:09 +0100

    Add king support

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import "./App.css";
 
 enum BoardProp {
   NONE,
-  RED_QUEEN,
-  RED_KING,
-  BLACK_QUEEN,
-  BLACK_KING,
+  ONE_QUEEN,
+  ONE_KING,
+  TWO_QUEEN,
+  TWO_KING,
   SUGGESTION,
 }
 
 enum Turn {
-  RED,
-  BLACK,
+  ONE,
+  TWO,
 }
+
+type PossibleMove = 7 | 9 | -7 | -9;
 
 function App() {
   // prettier-ignore
@@ -27,108 +29,123 @@ function App() {
     0, 3, 0, 3, 0, 3, 0, 3,
     3, 0, 3, 0, 3, 0, 3, 0,
   ]);
-  const [turn, setTurn] = useState<Turn>(Turn.RED);
+  const [turn, setTurn] = useState<Turn>(Turn.ONE);
   const [active, setActive] = useState<number>(-1);
 
-  const isTurn = (cell: BoardProp) => {
-    switch (turn) {
-      case Turn.RED:
-        return cell === BoardProp.RED_QUEEN || cell === BoardProp.RED_KING;
-      case Turn.BLACK:
-        return cell === BoardProp.BLACK_QUEEN || cell === BoardProp.BLACK_KING;
-    }
-  };
-
-  const isOther = (cell: BoardProp) => {
-    switch (turn) {
-      case Turn.RED:
-        return cell === BoardProp.BLACK_QUEEN || cell === BoardProp.BLACK_KING;
-      case Turn.BLACK:
-        return cell === BoardProp.RED_QUEEN || cell === BoardProp.RED_KING;
-    }
-  };
-
-  const isNotEdge = (index: number, x: 7 | 9) => {
-    switch (turn) {
-      case Turn.RED:
-        return (x === 7 && index % 8 !== 0) || (x === 9 && index % 8 !== 7);
-      case Turn.BLACK:
-        return (x === 9 && index % 8 !== 0) || (x === 7 && index % 8 !== 7);
-    }
-  };
-
-  const isNotJumpEdge = (index: number, x: 7 | 9) => {
-    switch (turn) {
-      case Turn.RED:
-        return (
-          (x === 7 && index % 8 !== 0 && index % 8 !== 1) ||
-          (x === 9 && index % 8 !== 7 && index % 8 !== 6)
-        );
-      case Turn.BLACK:
-        return (
-          (x === 9 && index % 8 !== 0 && index % 8 !== 1) ||
-          (x === 7 && index % 8 !== 7 && index % 8 !== 6)
-        );
-    }
-  };
-
-  const setBoardProp = (
-    index: number,
-    prop: BoardProp,
-    expected: BoardProp | null = null
-  ) => {
-    if (index < board.length - 1) {
-      if (expected === null || board[index] === expected) {
-        board[index] = prop;
-      }
-    }
-  };
-
-  const setSuggestions = (index: number, x: 7 | 9) => {
-    const multiplier = Turn.RED === turn ? 1 : -1;
-    const target = board[index + x * multiplier];
-
-    if (target === BoardProp.NONE) {
-      if (isNotEdge(index, x)) {
-        setBoardProp(
-          index + x * multiplier,
-          BoardProp.SUGGESTION,
-          BoardProp.NONE
-        );
-      }
-    } else if (isOther(target)) {
-      if (isNotJumpEdge(index, x)) {
-        setBoardProp(
-          index + 2 * x * multiplier,
-          BoardProp.SUGGESTION,
-          BoardProp.NONE
-        );
-      }
-    }
-  };
-
-  const resetSuggestions = () => {
-    for (const key in board) {
-      if (board[key] === BoardProp.SUGGESTION) {
-        board[key] = BoardProp.NONE;
-      }
-    }
-  };
-
   const onClick = (cell: BoardProp, index: number) => {
+    const setBoardProp = (
+      index: number,
+      prop: BoardProp,
+      expected: BoardProp | null = null
+    ) => {
+      if (index < board.length - 1) {
+        if (expected === null || board[index] === expected) {
+          board[index] = prop;
+        }
+      }
+    };
+
+    const isPlayerTurn = (cell: BoardProp) => {
+      switch (turn) {
+        case Turn.ONE:
+          return cell === BoardProp.ONE_QUEEN || cell === BoardProp.ONE_KING;
+        case Turn.TWO:
+          return cell === BoardProp.TWO_QUEEN || cell === BoardProp.TWO_KING;
+      }
+    };
+
+    const isOtherPlayer = (cell: BoardProp) => {
+      switch (turn) {
+        case Turn.ONE:
+          return cell === BoardProp.TWO_QUEEN || cell === BoardProp.TWO_KING;
+        case Turn.TWO:
+          return cell === BoardProp.ONE_QUEEN || cell === BoardProp.ONE_KING;
+      }
+    };
+
+    const isNotEdge = (index: number, x: PossibleMove) => {
+      switch (turn) {
+        case Turn.ONE:
+          return (
+            (x === 7 && index % 8 !== 0) ||
+            (x === 9 && index % 8 !== 7) ||
+            (x === -9 && index % 8 !== 0) ||
+            (x === -7 && index % 8 !== 7)
+          );
+        case Turn.TWO:
+          return (
+            (x === 9 && index % 8 !== 0) ||
+            (x === 7 && index % 8 !== 7) ||
+            (x === -7 && index % 8 !== 0) ||
+            (x === -9 && index % 8 !== 7)
+          );
+      }
+    };
+
+    const isNotJumpEdge = (index: number, x: PossibleMove) => {
+      switch (turn) {
+        case Turn.ONE:
+          return (
+            (x === 7 && index % 8 !== 0 && index % 8 !== 1) ||
+            (x === 9 && index % 8 !== 7 && index % 8 !== 6) ||
+            (x === -9 && index % 8 !== 0 && index % 8 !== 1) ||
+            (x === -7 && index % 8 !== 7 && index % 8 !== 6)
+          );
+        case Turn.TWO:
+          return (
+            (x === 9 && index % 8 !== 0 && index % 8 !== 1) ||
+            (x === 7 && index % 8 !== 7 && index % 8 !== 6) ||
+            (x === -7 && index % 8 !== 0 && index % 8 !== 1) ||
+            (x === -9 && index % 8 !== 7 && index % 8 !== 6)
+          );
+      }
+    };
+    const resetSuggestions = () => {
+      for (const key in board) {
+        if (board[key] === BoardProp.SUGGESTION) {
+          board[key] = BoardProp.NONE;
+        }
+      }
+    };
+
+    const setSuggestions = (index: number, x: PossibleMove) => {
+      const multiplier = Turn.ONE === turn ? 1 : -1;
+      const target = board[index + x * multiplier];
+
+      if (target === BoardProp.NONE) {
+        if (isNotEdge(index, x)) {
+          setBoardProp(
+            index + x * multiplier,
+            BoardProp.SUGGESTION,
+            BoardProp.NONE
+          );
+        }
+      } else if (isOtherPlayer(target)) {
+        if (isNotJumpEdge(index, x)) {
+          setBoardProp(
+            index + 2 * x * multiplier,
+            BoardProp.SUGGESTION,
+            BoardProp.NONE
+          );
+        }
+      }
+    };
+
     if (index === active) {
-      console.log("reset");
       setActive(-1);
       resetSuggestions();
       setBoard([...board]);
       return;
     }
 
-    if (isTurn(cell)) {
-      console.log("turn");
+    if (isPlayerTurn(cell)) {
       setActive(index);
       setSuggestions(index, 7);
       setSuggestions(index, 9);
+      if (cell === BoardProp.ONE_KING || cell === BoardProp.TWO_KING) {
+        setSuggestions(index, -7);
+        setSuggestions(index, -9);
+      }
       setBoard([...board]);
       return;
     }
@@ -145,10 +162,15 @@ function App() {
       }
 
       board[index] = board[active];
+      if (turn === Turn.ONE ? index >= 56 : index <= 7) {
+        board[index] =
+          turn === Turn.ONE ? BoardProp.ONE_KING : BoardProp.TWO_KING;
+      }
+
       board[active] = BoardProp.NONE;
       resetSuggestions();
       setBoard([...board]);
-      setTurn(turn === Turn.RED ? Turn.BLACK : Turn.RED);
+      setTurn(turn === Turn.ONE ? Turn.TWO : Turn.ONE);
       setActive(-1);
     }
   };
@@ -164,9 +186,12 @@ function App() {
           <div
             onClick={() => onClick(cell, index)}
             key={`${index}-${cell}`}
-            className={`App-box ${
-              cell === BoardProp.SUGGESTION ? "App-suggestion" : ""
-            }${active === index ? "App-active" : ""} App-player${cell}`}
+            className={
+              "App-box" +
+              (cell === BoardProp.SUGGESTION ? " App-suggestion" : "") +
+              (active === index ? " App-active" : "") +
+              ` App-player${cell}`
+            }
           />
         ))}
       </div>
